af947305697145654732252024db91260264cf20
John Ingve Olsen <johningveolsen@gmail.com>
2023-12-01 18:52:12 +0100

    Day1 part2

--- day1/day1.go
+++ day1/day1.go
@@ -13,17 +13,10 @@ var input string
 
 func main() {
 	fmt.Println(Part1(strings.NewReader(input)))
+	fmt.Println(Part2(strings.NewReader(input)))
 }
 
-func isDigit(c byte) bool {
-	return c >= '0' && c <= '9'
-}
-
-func toDigit(c byte) uint64 {
-	return uint64(c - '0')
-}
-
-func Part1(input io.Reader) uint64 {
+func solve(input io.Reader, getDigit func(str string, i int) (uint64, bool)) uint64 {
 	var sum uint64 = 0
 
 	scanner := bufio.NewScanner(input)
@@ -35,16 +28,65 @@ func Part1(input io.Reader) uint64 {
 
 		line := scanner.Text()
 		for i := range line {
-			if isDigit(line[i]) {
-				if firstDigit == 0 {
-					firstDigit = toDigit(line[i])
-				}
-				lastDigit = toDigit(line[i])
+			digit, ok := getDigit(line, i)
+			if !ok {
+				continue
 			}
+			if firstDigit == 0 {
+				firstDigit = digit
+			}
+			lastDigit = digit
 		}
 
 		sum += 10*firstDigit + lastDigit
 	}
 
 	return sum
+}
+
+func getDigit(str string, i int) (uint64, bool) {
+	if str[i] >= '1' && str[i] <= '9' {
+		return uint64(str[i] - '0'), true
+	}
+	return 0, false
+}
+
+func Part1(input io.Reader) uint64 {
+	return solve(input, getDigit)
+}
+
+var digits = map[string]uint64{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+func getSpelledDigit(str string, i int) (uint64, bool) {
+
+	for k, v := range digits {
+		if len(str)-i < len(k) {
+			continue
+		}
+		if str[i:i+len(k)] == k {
+			return v, true
+		}
+	}
+	return 0, false
+}
+
+func Part2(input io.Reader) uint64 {
+
+	return solve(input, func(str string, i int) (uint64, bool) {
+		d, ok := getDigit(str, i)
+		if ok {
+			return d, ok
+		}
+		return getSpelledDigit(str, i)
+	})
 }
--- day1/day1_test.go
+++ day1/day1_test.go
@@ -5,16 +5,31 @@ import (
 	"testing"
 )
 
-var testInput = `1abc2
+var inputPart1 = `1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet`
 
 func TestPart1(t *testing.T) {
 	var want uint64 = 142
-	got := Part1(strings.NewReader(testInput))
-
+	got := Part1(strings.NewReader(inputPart1))
 	if got != want {
 		t.Errorf("Part1() = %d, want %d", got, want)
+	}
+}
+
+var inputPart2 = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`
+
+func TestPart2(t *testing.T) {
+	var want uint64 = 281
+	got := Part2(strings.NewReader(inputPart2))
+	if got != want {
+		t.Errorf("Part2() = %d, want %d", got, want)
 	}
 }
